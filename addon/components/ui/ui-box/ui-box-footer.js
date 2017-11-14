38a09b89ccfb92db5c195b2b825133d1a823c183
Sarah Daily <sjdaily4@gmail.com>
2017-11-14 12:56:57 -0500

    Removed markup from template and modified box child components

--- addon/components/ui/ui-box/ui-box-footer.js
+++ addon/components/ui/ui-box/ui-box-footer.js
@@ -1,3 +1,7 @@
 import Component from '@ember/component';
 
-export default Component.extend({});
+export default Component.extend({
+  classNames: ['card-footer'],
+  classNameBindings: ['contentModifier'],
+  contentModifier: false,
+});
--- addon/components/ui/ui-box/ui-box-list.js
+++ addon/components/ui/ui-box/ui-box-list.js
@@ -1,3 +1,8 @@
 import Component from '@ember/component';
 
-export default Component.extend({});
+export default Component.extend({
+  tagName: 'ul',
+  classNames: ['list-group', 'list-group-flush'],
+  classNameBindings: ['contentModifier'],
+  contentModifier: false,
+});
