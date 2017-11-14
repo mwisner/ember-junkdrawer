fbc95817048e74351516d591c2ab65f19aec7a31
Sarah Daily <sjdaily4@gmail.com>
2017-11-14 13:01:15 -0500

    Added tagname to box header

--- addon/components/ui/ui-box/ui-box-header.js
+++ addon/components/ui/ui-box/ui-box-header.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: 'h5',
   classNames: ['card-header'],
   classNameBindings: ['contentModifier'],
   contentModifier: false,
