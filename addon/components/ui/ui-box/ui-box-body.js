6d4b98ee77f4dd49684d592f1c1c784dc22f5a4a
Sarah Daily <sjdaily4@gmail.com>
2017-11-14 08:30:02 -0500

    Testing initial ui-box refactor

---
+++ addon/components/ui/ui-box/ui-box-body.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../../templates/ui/ui-box/ui-box-body';
+
+export default Component.extend({
+  layout,
+
+  // Component options logic here
+});
