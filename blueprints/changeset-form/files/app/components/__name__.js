99897266f8985dc0507a2bc43c23c10e3f3392ce
Donald Wasserman <djwasserman@gmail.com>
2017-12-13 12:43:52 -0500

    removed unneeded imports from blueprint

--- blueprints/changeset-form/files/app/components/__name__.js
+++ blueprints/changeset-form/files/app/components/__name__.js
@@ -1,7 +1,5 @@
 import { inject as service } from '@ember/service';
 import { set, get } from '@ember/object';
-import Changeset from 'ember-changeset';
-import lookupValidator from 'ember-changeset-validations';
 import FormComponent from 'ember-junkdrawer/components/form/changeset-form';
 
 
