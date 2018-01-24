4433e795737e39a68244c5527da16efc12b4edd5
Donald Wasserman <djwasserman@gmail.com>
2018-01-24 12:25:29 -0500

    better error handling

--- addon/components/ui/ui-box/ui-box-img.js
+++ addon/components/ui/ui-box/ui-box-img.js
@@ -1,13 +1,17 @@
 import Component from '@ember/component';
 import {assert, warn} from '@ember/debug';
-import {get} from '@ember/object';
+import {computed, get} from '@ember/object';
 
 
 export default Component.extend({
   layout,
-  classNames: ['card-img-top'],
+  classNameBindings: ['_position'],
   attributeBindings: ['src', 'alt'],
   tagName: 'img',
+  position: 'top',
+  _position: computed('position', function() {
+    return `card-img-${get(this, 'position')}`;
+  })
   src: false,
   alt: false,
   init() {
@@ -15,6 +19,7 @@ export default Component.extend({
     assert('You musn\'t modify `tagName` to be anything other than `img`',
             get(this, 'tagName') === 'img');
     assert('You must provide a `src` attribute', get(this, 'src'));
-    warn('You really should provide an `alt`-text attribute if you care about the future of this internet thing', get(this, 'src'))
+    assert('`position` must be either `top` or `bottom`', ['top', 'bottom'].includes(get(this, 'position')));
+    warn('You really should provide an `alt`-text attribute if you care about the future of this internet thing', get(this, 'src'));
   }
 });
