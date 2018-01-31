e888c55185783069791ca6bdc690513b9985b530
Donald Wasserman <djwasserman@gmail.com>
2018-01-31 09:39:39 -0500

    updated changeset form default template

--- blueprints/changeset-form/files/app/components/__name__.js
+++ blueprints/changeset-form/files/app/components/__name__.js
@@ -1,8 +1,16 @@
 import { inject as service } from '@ember/service';
-import { set, get } from '@ember/object';
+import { computed, set, get } from '@ember/object';
 import FormComponent from 'ember-junkdrawer/components/form/changeset-form';
 
 
 export default FormComponent.extend({
-  store: service(),
+  flashMessages: service(),
+  model: computed(function() {
+    // Return your model here
+    return false;
+  }),
+
+  onSubmitSuccess() {
+    get(this, 'flashMessages').success('Organization Updated');
+  },
 });
