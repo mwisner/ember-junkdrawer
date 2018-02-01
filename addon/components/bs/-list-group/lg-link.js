b3e9c20f6420da6fb67920f93f9e49756d262ce7
Donald Wasserman <djwasserman@gmail.com>
2018-02-01 13:42:28 -0500

    added link button to list group

---
+++ addon/components/bs/-list-group/lg-link.js
@@ -0,0 +1,5 @@
+import LinkComponent from '@ember/routing/link-component';
+
+export default LinkComponent.extend({
+  classNames: ['list-group-item', 'list-group-item-action'],
+});
---
+++ app/components/bs/-list-group/lg-link.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-junkdrawer/components/bs/-list-group/lg-link';
