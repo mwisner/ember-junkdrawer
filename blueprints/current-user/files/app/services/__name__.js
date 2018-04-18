9375832ab316142ecab69036ef022ab2c85b2d76
Donald Wasserman <djwasserman@gmail.com>
2018-04-18 09:53:26 -0400

    removed uneeded imports from blueprint

--- blueprints/current-user/files/app/services/__name__.js
+++ blueprints/current-user/files/app/services/__name__.js
@@ -1,6 +1,5 @@
 import CurrentUser from 'ember-junkdrawer/services/current-user';
 import { inject as service } from '@ember/service';
-import { get, set } from '@ember/object';
 
 export default CurrentUser.extend({
     didSetupUser(user) {},
