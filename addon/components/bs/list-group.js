7ffa288ed6a20ca535a12eab185ef08e2f665726
Matt Wisner <matt@detroitweb.company>
2018-04-24 21:31:05 -0400

    Removing broken docs

--- addon/components/bs/list-group.js
+++ addon/components/bs/list-group.js
@@ -4,19 +4,7 @@ import layout from '../../templates/components/bs/list-group';
 export default Component.extend({
   layout,
 
-  /**
-   * @public
-   * @property
-   * Control tag type, defaults as ul, 
-   * available types are `div`, `ul`
-   */
   type: 'ul',
-  
-  /**
-   * @public
-   * @property
-   * control flush property
-   */
   flush: false,
   tagNameBinding: ['type'],
   classNamaes: ['list-group'],
