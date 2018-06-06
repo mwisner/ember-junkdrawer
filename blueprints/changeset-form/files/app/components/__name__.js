346657a3a971048b509a3b1df5afc121d51cd276
Donald Wasserman <djwasserman@gmail.com>
2018-06-06 07:36:06 -0400

    more trying to get tests to pass

--- blueprints/changeset-form/files/app/components/__name__.js
+++ blueprints/changeset-form/files/app/components/__name__.js
@@ -1,5 +1,5 @@
 import FormComponent from 'ember-junkdrawer/components/form/changeset-form';
-
+import { computed } form '@ember/object';
 
 export default FormComponent.extend({
   model: computed(function() {
