20df8b934dc9e8e4e1cede70c0616c4eb9ce6438
Donald Wasserman <djwasserman@gmail.com>
2018-12-09 08:58:36 -0500

    More reference updates

--- addon/components/bs-card.js
+++ addon/components/bs-card.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import layout from '../templates/components/ui-card';
+import layout from '../templates/components/bs-card';
 
 export default Component.extend({
   layout,
--- tests/integration/components/bs-card-test.js
+++ tests/integration/components/bs-card-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | ui-card', function(hooks) {
+module('Integration | Component | bs-card', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
